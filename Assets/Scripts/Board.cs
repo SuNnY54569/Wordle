ed6dac3034c821b9a3cc86fb776295b6439fc89f
Sorawis Leepiyasakulchai <sorawisson@gmail.com>
2025-03-26 22:38:03 +0700

    Type in letters

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,17 +1,58 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class Board : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static readonly KeyCode[] SUPPORTED_KEYS = new KeyCode[]
     {
-        
+        KeyCode.A, KeyCode.B, KeyCode.C, KeyCode.D, KeyCode.E, KeyCode.F,
+        KeyCode.G, KeyCode.H, KeyCode.I, KeyCode.J, KeyCode.K, KeyCode.L,
+        KeyCode.M, KeyCode.N, KeyCode.O, KeyCode.P, KeyCode.Q, KeyCode.R,
+        KeyCode.S, KeyCode.T, KeyCode.U, KeyCode.V, KeyCode.W, KeyCode.X,
+        KeyCode.Y, KeyCode.Z
+    };
+
+    private Row[] rows;
+
+    private int rowIndex;
+    private int columnIndex;
+
+    private void Awake()
+    {
+        rows = GetComponentsInChildren<Row>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
+    {
+        Row currentRow = rows[rowIndex];
+        
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            columnIndex = Mathf.Max(columnIndex - 1, 0);
+            currentRow.tiles[columnIndex].SetLetter('\0');
+        }
+        else if (columnIndex >= rows[rowIndex].tiles.Length)
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                SubmitRow(currentRow);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < SUPPORTED_KEYS.Length; i++)
+            {
+                if (Input.GetKeyDown(SUPPORTED_KEYS[i]))
+                {
+                    rows[rowIndex].tiles[columnIndex].SetLetter((char)SUPPORTED_KEYS[i]);
+                    columnIndex++;
+                    break;
+                }
+            }
+        }
+    }
+
+    private void SubmitRow(Row row)
     {
         
     }
--- Assets/Scripts/Row.cs
+++ Assets/Scripts/Row.cs
@@ -1,18 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Row : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Tile[] tiles { get; private set; }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        tiles = GetComponentsInChildren<Tile>();
     }
 }
